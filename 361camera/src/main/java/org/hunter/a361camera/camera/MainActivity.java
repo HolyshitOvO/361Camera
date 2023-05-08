5ed00d730ee993db17bca3895a4e6b9099a726a2
HolyshitOvO <39211450+HolyshitOvO@users.noreply.github.com>
2023-05-08 11:41:00 +0800

    2023-05-08

--- 361camera/src/main/java/org/hunter/a361camera/camera/MainActivity.java
+++ 361camera/src/main/java/org/hunter/a361camera/camera/MainActivity.java
@@ -1,6 +1,10 @@
 package org.hunter.a361camera.camera;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import org.hunter.a361camera.R;
@@ -9,17 +13,30 @@ import org.hunter.a361camera.view.Camera2Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private static final String[] NEEDED_PERMISSIONS = new String[]{
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			Manifest.permission.CAMERA,
+	};
+	private static final int ACTION_REQUEST_PERMISSIONS = 1;
 
-        Camera2Fragment camera2Fragment = (Camera2Fragment) getFragmentManager()
-                .findFragmentById(R.id.contentFrame);
-        if (camera2Fragment == null) {
-            camera2Fragment = Camera2Fragment.newInstance();
-            ActivityUtils.addFragmentToActivity(getFragmentManager(),
-                    camera2Fragment, R.id.contentFrame);
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (ContextCompat.checkSelfPermission(this,NEEDED_PERMISSIONS[0]) != PackageManager.PERMISSION_GRANTED ||
+				ContextCompat.checkSelfPermission(this,NEEDED_PERMISSIONS[1]) != PackageManager.PERMISSION_GRANTED ||
+				ContextCompat.checkSelfPermission(this,NEEDED_PERMISSIONS[2]) != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(this,NEEDED_PERMISSIONS,ACTION_REQUEST_PERMISSIONS);
+		}
+
+		Camera2Fragment camera2Fragment = (Camera2Fragment) getFragmentManager()
+				.findFragmentById(R.id.contentFrame);
+		if (camera2Fragment == null) {
+			camera2Fragment = Camera2Fragment.newInstance();
+			ActivityUtils.addFragmentToActivity(getFragmentManager(),
+					camera2Fragment,R.id.contentFrame);
+		}
+	}
 }
